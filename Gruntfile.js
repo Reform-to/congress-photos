9e5f871e125daa00e7e8cc8bb0123296ee4b4a2e
Christopher Adams <chris@raysend.com>
2013-12-26 00:27:45 +0800

    Add copy tasks for images

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,14 @@ module.exports = function(grunt) {
       small: {
         src: 'http://assets.sunlightfoundation.com/moc/40x50.zip',
         dest: 'tmp/zip/40x50.zip'
+      },
+      medium: {
+        src: 'http://assets.sunlightfoundation.com/moc/100x125.zip',
+        dest: 'tmp/zip/100x125.zip'
+      },
+      large: {
+        src: 'http://assets.sunlightfoundation.com/moc/200x250.zip',
+        dest: 'tmp/zip/200x250.zip'
       }
     },
 
@@ -13,19 +21,38 @@ module.exports = function(grunt) {
       small: {
         src: 'tmp/zip/40x50.zip',
         dest: 'tmp/src'
-      }
+      },
+      medium: {
+        src: 'tmp/zip/100x125.zip',
+        dest: 'tmp/src'
+      },
+      large: {
+        src: 'tmp/zip/200x250.zip',
+        dest: 'tmp/src'
+      },
     },
 
     copy: {
       small: {
         files: [
-          { expand: true, src: ['tmp/src/40x50/**'], dest: 'dist/assets/' },
+          { expand: true, cwd: 'tmp/src/', src: ['40x50/**'], dest: 'dist/' },
         ]
-      }
+      },
+      medium: {
+        files: [
+          { expand: true, cwd: 'tmp/src/', src: ['100x125/**'], dest: 'dist/' },
+        ]
+      },
+      large: {
+        files: [
+          { expand: true, cwd: 'tmp/src/', src: ['200x250/**'], dest: 'dist/' },
+        ]
+      },
     },
 
     clean: {
         tmp: ['tmp'],
+        dist: ['dist']
     }
 
   });
@@ -35,7 +62,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
 
-  grunt.registerTask('build', ['curl', 'unzip']);
-  grunt.registerTask('default', ['build', 'clean']);
-  grunt.registerTask('open', ['unzip']);
+  grunt.registerTask('fetch', ['clean:tmp', 'curl', 'unzip']);
+  grunt.registerTask('build', ['clean:dist', 'copy']);
+  grunt.registerTask('default', ['fetch', 'build']);
 }
